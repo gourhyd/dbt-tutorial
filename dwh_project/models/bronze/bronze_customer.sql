051e69b192aac72bf140c63ba3b4679c811284d3
Gour Chandra <gour.chandra@hotmail.com>
2025-10-02 00:28:06 +0530

    save

--- dwh_project/models/bronze/bronze_customer.sql
+++ dwh_project/models/bronze/bronze_customer.sql
@@ -1,2 +1,14 @@
+{{
+    config(
+    materialized = 'incremental',
+    unique_key = 'customer_sk'
+    )
+}}
+
 select * from
 {{source('netfilxdb','dim_customer')}}
+
+
+{% if is_incremental() %}
+  where customer_sk > (select max(customer_sk) from {{this}})
+{% endif %}
