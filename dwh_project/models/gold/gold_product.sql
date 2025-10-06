ad6040037d784547c0b5adc36bf8f87ac03985dc
Gour Chandra <gour.chandra@hotmail.com>
2025-10-06 11:41:34 +0530

    save new file

--- dwh_project/models/gold/gold_product.sql
+++ dwh_project/models/gold/gold_product.sql
@@ -1,1 +1,11 @@
-select * from netfilxdb.gangadwh.dim_product
+-- select * from netfilxdb.gangadwh.dim_product
+
+{{ config(
+   materialized = 'table'
+) }}
+
+{{ audit_helper.compare_all_columns (
+   a_relation = ref('bronze_cust'),
+   b_relation = ref('bronze_customer'),
+   primary_key = "CUSTOMER_SK"
+) }}
